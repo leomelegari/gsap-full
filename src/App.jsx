0af108eb0cebb24e592740f5dfac82130b056674
Leo Melegari <leomelegaridev@gmail.com>
2025-06-22 09:21:37 -0300

    up Appjsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Cocktails from "./components/cocktails";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+//  apenas para upload
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+    </main>
+  );
+};
+
+export default App;
